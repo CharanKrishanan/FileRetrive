d895c0bacfcbe6aedfcf398ef628157d34ea726e
CharanKrishnan <charankovvali@gmail.com>
2023-10-23 19:45:54 +0530

    Add project files.

---
+++ FileRetrive/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileRetrive
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            FileStream fs = new FileStream("C:\\Users\\Dell\\Documents\\Practice\\.NET\\FileRetrive\\Students_Data.txt", FileMode.Open,FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            String data=sr.ReadToEnd();
+            Console.WriteLine(data);
+            sr.Close();
+            sr.Dispose();
+            fs.Close();
+            fs.Dispose();
+            Console.ReadLine();
+
+        }
+    }
+}
